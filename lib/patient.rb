965aff0d5f1a430dd365363df94c3afd1654d50c
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-15 14:17:14 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,7 +8,8 @@ def initialize(name)
 end
 
 def new_appointment(date, doctor)
-  Appointment.new(date, self, doctor)
+  @date = date.to_str
+  Appointment.new(@date, self, doctor)
 end
 
 def self.all
