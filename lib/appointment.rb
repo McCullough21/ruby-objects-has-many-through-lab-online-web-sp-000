6575773f535b0efe0e78da2d392786a7ed26dd6d
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-15 14:16:21 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ attr_accessor :doctor, :patient, :date, :appointment
 
 def initialize(date, patient, doctor)
   @date = date
-  self.patient = patient
+  @patient = patient
   @doctor = doctor
   @@all << self
 end
