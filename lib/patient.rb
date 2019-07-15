15b5f2195859e6489f638d85f89c6ffed6c5835a
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-15 13:34:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,15 @@
 class Patient
+attr_accessor :name, :appointment, :doctor, :date
+@@all = []
 
+def initialize(name)
+  @name = name
+  @@all << self
+end
 
+def new_appointment(doctor, date)
+  Appointment.new(date, doctor, self)
+end
 
 
 end
